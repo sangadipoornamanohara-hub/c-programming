e42818e7e4e907c26f8410d93377f289e56a067d
sangadipoornamanohara-hub <sangadipoornamanohara@gmail.com>
2025-11-06 21:59:31 +0530

    Create area and perimeter of square.cpp
    file is created

---
+++ area and perimeter of square.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int s, a, p;
+    printf("enter side of square");
+    scanf("%d",&s);
+    a=s*s;
+    p=4*s;
+    printf("area and perimeter of square is %d %d",a,p);
+    return 0;
+}
+
